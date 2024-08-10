10c52ce0db2071e18a0e9bbe79e82a0aefc41485
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-08-11 02:47:06 +0700

    Làm trang topics bên client

--- controller/client/topic.controller.ts
+++ controller/client/topic.controller.ts
@@ -7,9 +7,9 @@ const topics = async (req : Request, res : Response) => {
         deleted : false,
     })
 
-
     res.render("client/pages/topics/index.pug", {
         pageTitle : "Chủ đề bài hát",
+        topics : topics,
     })
 }
 
